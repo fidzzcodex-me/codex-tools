002a86055dacf5fb5db2635057284ff701dcf32e
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-25 11:20:04 +0700

    upload: scripts/git-setup.sh

--- scripts/git-setup.sh
+++ scripts/git-setup.sh
@@ -14,21 +14,14 @@ setup_git_repo() {
     local branch_flag=""
     [ -n "$BRANCH" ] && branch_flag="-b $BRANCH"
     git clone $branch_flag "$repo_url" temp_clone 2>&1 | grep -v "$ACCESS_TOKEN"
-    local clone_status="${PIPESTATUS[0]}"
-    if [ "$clone_status" -ne 0 ] || [ ! -d "temp_clone" ]; then
-      ui_err "[git] Clone gagal (exit ${clone_status}). Cek GIT_ADDRESS/USERNAME/ACCESS_TOKEN/BRANCH di panel -- container tetap kosong."
+    if [ -d "temp_clone" ]; then
+      shopt -s dotglob
+      mv temp_clone/* . 2>/dev/null
       rm -rf temp_clone
-      return 1
+      shopt -u dotglob
     fi
-    shopt -s dotglob
-    mv temp_clone/* . 2>/dev/null
-    rm -rf temp_clone
-    shopt -u dotglob
-    ui_ok "[git] Clone selesai"
   elif [ "$AUTO_UPDATE" = "true" ]; then
     echo -e "${C_SKY}[git] Pulling latest changes...${C_RESET}"
     git pull 2>&1 | grep -v "$ACCESS_TOKEN"
-    local pull_status="${PIPESTATUS[0]}"
-    [ "$pull_status" -ne 0 ] && ui_err "[git] Pull gagal (exit ${pull_status})"
   fi
 }
